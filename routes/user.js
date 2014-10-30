1fcb924a9a4d28e7043e7923a9a8244cabe0fe80
Juuso Hatakka <rasek24@gmail.com>
2014-10-30 19:30:01 +0200

    add async module

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,69 @@
 var router = require('express').Router();
 var passport = require('passport');
+var async = require('async');
 
 var User = require('../models/user.js');
 var Location = require('../models/location.js');
 var Collection = require('../models/collection.js');
+var Image = require('../models/image.js');
 
 router.get('/users/:id', function(req, res) {
 
 	User.findById(req.params.id, function(err, doc) {
-		if (err) {
-			res.status(400).json(err);
-		}
+		if (err)
+			res.status(400).json({message: err.message});
 
-		if (!doc) {
-			//res.json()
-		}
+		if (!doc)
+			res.status(400).json({message: "Not found"});
 
-		res.json({username: doc.username});
+		async.parallel([
+
+			//Get user's locations
+			function(callback) {
+				Location.find({owners: doc.id}, '_id title', function(err, locations) {
+					if (err)
+						callback(err)
+
+					callback(null, locations);
+				});
+			},
+
+			//Get user's collections
+			function(callback) {
+				Collection.find({owners: doc.id}, '_id title', function(err, collections) {
+					if (err)
+						callback(err)
+
+					callback(null, collections);
+				});
+			},
+
+			//Get user's images
+			function(callback) {
+				Image.find({owner: doc.id}, '_id', function(err, images) {
+					if (err)
+						callback(err)
+
+					callback(null, images);
+				});
+			}
+
+		],
+
+		//Handle results
+		function(err, results) {
+
+			if (err)
+				return res.status(400).json({message: err.message});
+
+			var user = {};
+			user.username = doc.username;
+			user.locations = results[0] || [];
+			user.collections = results[1] || [];
+			user.images = results[2] || [];
+
+			res.json(user);
+		});
 
 	});
 
