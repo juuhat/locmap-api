868711cbd54f90aaf49c432f06c6a149428b42c3
Juuso Hatakka <rasek24@gmail.com>
2014-11-04 15:44:45 +0200

    Update bearer.js

--- config/strategies/bearer.js
+++ config/strategies/bearer.js
@@ -1,4 +1,5 @@
 var BearerStrategy = require('passport-http-bearer').Strategy;
+var vars = require('../variables.js');
 
 module.exports = new BearerStrategy(function(token, done) {
 	var User = require('../../models/user.js');
