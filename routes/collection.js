f5395bfd4ff60514d007ad73e9a9bc3a9c4ea4f5
Juuso Hatakka <rasek24@gmail.com>
2014-11-06 13:32:16 +0200

    get full data from locations attached to collection

--- routes/collection.js
+++ routes/collection.js
@@ -1,19 +1,56 @@
 var router = require('express').Router();
 var passport = require('passport');
+var async = require('async');
 
 var Collection = require('../models/collection.js');
+var Location = require('../models/location.js');
+var Image = require('../models/image.js');
 
 //GET
 //Get collection specified by id
 router.get('/collections/:id', function(req, res) {
-	Collection.findById(req.params.id, function(err, doc) {
+	Collection.findById(req.params.id, function(err, collection) {
 		if (err)
 			return res.status(400).json({message: err.message});
 
-		if (!doc)
+		if (!collection)
 			return res.status(400).json({message: "Not found"});
 
-		res.json(doc);
+		//Find locations attached to collection
+		Location.find({'_id': { $in: collection.locations}}, function(err, locations) {
+  			
+  			//Find images attached to location
+  			async.map(locations, function(loc, done) {
+  				Image.find({location: loc.id}, '_id', function(err, images) {
+  			    	if (err)
+  			      		done(err, null);
+
+  			    	var loc2 = loc.toObject();
+  			    	delete loc2.__v;
+
+  			    	loc2.images = [];
+  			    	images.forEach(function(e) {
+  			    		loc2.images.push(e.id);
+  			    	});
+
+  			    	done(null, loc2);
+  			});
+  			},
+  			
+  			//Handle results
+  			function(err, results) {
+  			  if (err)
+  			    return res.status(400).json({message: err.message});
+
+  				var collection2 = collection.toObject();
+  				delete collection2.__v;
+  				collection2.locations = results;
+
+  			    res.json(collection2);
+  			});
+
+		});
+
 	});
 });
 
