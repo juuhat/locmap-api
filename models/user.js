cffd171ff8c5a7322495168073601f90da512176
Juuso Hatakka <rasek24@gmail.com>
2014-11-06 14:24:37 +0200

    changed username to required field

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	username: String,
+	username: {type: String, required: true, unique: true},
 	email: {type: String, required: true, unique: true},
 	password: {type: String, required: true},
 	token: String,
