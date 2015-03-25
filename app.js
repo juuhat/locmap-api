9f62943f5c8a154e416d95a04c61d8f42747e63e
Juuso Hatakka <rasek24@gmail.com>
2015-03-25 18:52:15 +0200

    added support for test env varible

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 var express    = require('express');
-var app        = express();
+var express        = express();
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var passport   = require('./config/passport');
 var vars       = require('./config/variables.js');
 
-app.use(passport.initialize());
+var isTestEnv = process.env.TEST;
 
-//configure app to use bodyParser()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+express.use(passport.initialize());
+
+//configure express to use bodyParser()
+express.use(bodyParser.urlencoded({ extended: true }));
+express.use(bodyParser.json());
 
 //error handler
-app.use(function(err, req, res, next) {
+express.use(function(err, req, res, next) {
 	return res.status(400).json({message: err.message});
 });
 
 //mongoDB connection
-mongoose.connect(vars.mongodbAddress);
+if (!isTestEnv) {
+	mongoose.connect(vars.mongodbAddress);
+}
 
 //set routes
-app.use(vars.apiPrefix, require('./routes/location.js'));
-app.use(vars.apiPrefix, require('./routes/collection.js'));
-app.use(vars.apiPrefix, require('./routes/auth.js'));
-app.use(vars.apiPrefix, require('./routes/user.js'));
-app.use(vars.apiPrefix, require('./routes/image.js'));
+express.use(vars.apiPrefix, require('./routes/location.js'));
+express.use(vars.apiPrefix, require('./routes/collection.js'));
+express.use(vars.apiPrefix, require('./routes/auth.js'));
+express.use(vars.apiPrefix, require('./routes/user.js'));
+express.use(vars.apiPrefix, require('./routes/image.js'));
+
+module.exports.app = express;
 
 //start server
-app.listen(vars.port);
-console.log('Listening on port: ' + vars.port);
+if (!isTestEnv) {
+	express.listen(vars.port);
+	console.log('Listening on port: ' + vars.port);
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,19 @@
 var supertest = require('supertest');
-var mongoose = require('mongoose');
-var mockgoose = require('mockgoose');
-var url = 'http://localhost:8080/api/v1';
+var mongoose   = require('mongoose');
+var mockgoose  = require('mockgoose');
+var url = 'http://localhost:4000/api/v1';
+
+process.env.TEST = true;
 
 before(function(done) {
+	//set up mongoose
+	mockgoose(mongoose);
+	//mongoose.connect('mongodb://localhost/locmap');
+	
+	//start server
+	var server = require('../app');
+	this.app = server.app.listen(4000);
+
 	done();
 });
 
@@ -14,4 +24,18 @@ describe('auth', function() {
 		.send({email: "legitq@@.com", password: "test1234", username: "qqlegit"})
 		.expect(400, done);
 	});
+
+	it('should register user with valid credentials', function(done) {
+		supertest(url)
+		.post('/auth/register')
+		.send({email: "tester@example.com", password: "Test@9321", username: "tester"})
+		.expect(200, done);
+	});
+
+});
+
+after(function() {
+	mockgoose.reset();
+	mongoose.connection.close();
+	this.app.close();
 });
