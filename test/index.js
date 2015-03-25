e045504cb470f365952bd50e4d77fdf6d55047c2
Juuso Hatakka <rasek24@gmail.com>
2015-03-25 21:25:28 +0200

    added more tests

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ process.env.TEST = true;
 before(function(done) {
 	//set up mongoose
 	mockgoose(mongoose);
-	//mongoose.connect('mongodb://localhost/locmap');
+	mongoose.connect('mongodb://localhost/locmap');
 	
 	//start server
 	var server = require('../app');
@@ -17,19 +17,78 @@ before(function(done) {
 	done();
 });
 
-describe('auth', function() {
-	it('should reject invalid email', function(done) {
-		supertest(url)
-		.post('/auth/register')
-		.send({email: "legitq@@.com", password: "test1234", username: "qqlegit"})
-		.expect(400, done);
+describe('LocMap API usage', function() {
+
+	var ctx = { };
+	ctx.user = { email: "tester@example.com", password: "Test@9321", username: "tester" };
+	ctx.location = { title: "Test", description: "Test description", latitude: 23.1, longitude: 62 };
+
+	//AUTH
+	describe('auth', function() {
+		it('should reject invalid email in register', function(done) {
+			supertest(url)
+			.post('/auth/register')
+			.send({email: "legitq@@.com", password: "test1234", username: "qqlegit"})
+			.expect(400, done);
+		});
+
+		it('should register user with valid credentials', function(done) {
+			supertest(url)
+			.post('/auth/register')
+			.send(ctx.user)
+			.expect(200, done);
+		});
+
+		it('should reject wrong password in login', function(done) {
+			supertest(url)
+			.post('/auth/login')
+			.send({email: ctx.user.email, password: "Test98765"})
+			.expect(400, done);
+		});
+
+		it('should login with correct credentials', function(done) {
+			supertest(url)
+			.post('/auth/login')
+			.send({email: ctx.user.email, password: ctx.user.password})
+			.expect(200, function(err, res) {
+				if (err)
+					return done(err)
+
+				ctx.user.token = res.headers['x-access-token'];
+				done();
+			});
+		});
 	});
 
-	it('should register user with valid credentials', function(done) {
-		supertest(url)
-		.post('/auth/register')
-		.send({email: "tester@example.com", password: "Test@9321", username: "tester"})
-		.expect(200, done);
+	//LOCATIONS
+	describe('location', function() {
+		it('should reject location creation with invalid auth token', function(done) {
+			supertest(url)
+			.post('/locations')
+			.set('Authorization', 'Bearer 1234567890')
+			.send(ctx.location)
+			.expect(401, done);
+		});
+
+		it('should create location with correct data', function(done) {
+			supertest(url)
+			.post('/locations')
+			.set('Authorization', 'Bearer ' + ctx.user.token)
+			.send(ctx.location)
+			.expect(200, function(err, res) {
+				if (err)
+					return done(err)
+
+				ctx.location.id = res.body._id;
+				done();
+			});
+		});
+
+		it('should get existing location with correct objectId', function(done) {
+			supertest(url)
+			.get('/locations/' + ctx.location.id)
+			.expect(200, done);
+		});
 	});
 
 });
